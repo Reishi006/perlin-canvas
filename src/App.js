fde4b6ca257b744154231cd75bedfff59322b6f3
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-06 18:44:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ const handleColors = () => {
 
 const distVectorLoop = () => {
   if (distanceVectors.length < 1) {
-    for (let i = 0; i < 10; i += pixelSize*10) {
-      for (let j = 0; j < 10; j += pixelSize*10) {
+    for (let i = 0; i < 80; i += pixelSize*10) {
+      for (let j = 0; j < 80; j += pixelSize*10) {
         calculateDistVec(Number((j/10).toFixed(1)), Number((i/10).toFixed(1)));
         //console.log(Number((j/10).toFixed(1)));
       }
@@ -38,11 +38,12 @@ const distVectorLoop = () => {
   console.log(distanceVectors);
 }
 
+//STEP.2: Calculate distance vectors for each pixels (4 vectors pointing to one pixel in one grid (2^nDim x onePixel x gridSize))
 const calculateDistVec = (x, y) => {
-  let topLeft = [Math.abs(x - normalizedPoints[0][0]).toFixed(2), Math.abs(y - normalizedPoints[0][1]).toFixed(2)];
-  let topRight = [Math.abs(x - normalizedPoints[1][0]).toFixed(2), Math.abs(y - normalizedPoints[1][1]).toFixed(2)];
-  let bottomRight = [Math.abs(x - normalizedPoints[2][0]).toFixed(2), Math.abs(y - normalizedPoints[2][1]).toFixed(2)];
-  let bottomLeft = [Math.abs(x - normalizedPoints[3][0]).toFixed(2), Math.abs(y - normalizedPoints[3][1]).toFixed(2)];
+  let topLeft = [Number(Math.abs(x - normalizedPoints[0][0]).toFixed(2)), Number(Math.abs(y - normalizedPoints[0][1]).toFixed(2))];
+  let topRight = [Number(Math.abs(x - normalizedPoints[1][0]).toFixed(2)), Number(Math.abs(y - normalizedPoints[1][1]).toFixed(2))];
+  let bottomRight = [Number(Math.abs(x - normalizedPoints[2][0]).toFixed(2)), Number(Math.abs(y - normalizedPoints[2][1]).toFixed(2))];
+  let bottomLeft = [Number(Math.abs(x - normalizedPoints[3][0]).toFixed(2)), Number(Math.abs(y - normalizedPoints[3][1]).toFixed(2))];
 
   distanceVectors.push(topLeft);
   distanceVectors.push(topRight);
@@ -52,11 +53,13 @@ const calculateDistVec = (x, y) => {
 
 distVectorLoop();
 
+
+//STEP.3: Calculate dot product of earlier calculated gradient vectors and distance vectors (D1 = a1b1 + a2b2)
 const calculateDotProduct = () => {
-  if (dotProduct.length < canvasSize * canvasSize) {
-    for (let i = 0; i < canvasSize; i++) {
-      for (let j = 0; j < 400; j++) {
-        let dot = (distanceVectors[j]?.[0] * seedGrad[i]?.[0]) + (distanceVectors[j]?.[1] * seedGrad[i]?.[1]);
+  if (dotProduct.length < (canvasSize * canvasSize)/recSize) {
+    for (let i = 0; i < seedGrad.length; i++) {
+      for (let j = 0; j < canvasSize/recSize; j++) {
+        let dot = (distanceVectors[j][0] * seedGrad[i][0]) + (distanceVectors[j][1] * seedGrad[i][1]);
         dotProduct.push(dot);
       }
     }
@@ -65,17 +68,25 @@ const calculateDotProduct = () => {
   console.log(distanceVectors[1][0]);
   console.log(seedGrad[0][0]);
   console.log((distanceVectors[1][0] * seedGrad[1][0]) + (distanceVectors[1][1] * seedGrad[1][1]));
+  console.log(`%c . dotProduct: `, 'color: orange;');
   console.log(dotProduct);
 }
 
-
+//STEP.4: Interpolate dot products from the step.3
 const interpolateDots = () => {
-  for (let i = 0; i < dotProduct.length; i += 1) {
-    let AB = dotProduct[i] + Number((pixelSize*10).toFixed(0)) * (dotProduct[i+1]);
-    let CD = dotProduct[i+3] + Number((pixelSize*10).toFixed(0)) * (dotProduct[i+2]);
-    let value = AB + Number((pixelSize*10).toFixed(0)) * (CD - AB);
-    value = (value - 0) / (3 - 0);
+  let k = 0;
+  for (let i = 0; i < dotProduct.length; i += 4) {
+    let fracX = distanceVectors[k][0] - Math.floor(distanceVectors[k][0]);
+    let fracY = distanceVectors[k][0] - Math.floor(distanceVectors[k][1]);
+
+    let AB = dotProduct[i] + fracX * (dotProduct[i+1] - dotProduct[i]);
+    let CD = dotProduct[i+3] + fracX * (dotProduct[i+2] - dotProduct[i+3]);
+    let value = AB + fracY * (CD - AB);
+    value = Math.abs((value - 0) / (5 - 0));
     colorValues.push(value);
+    k++;
+
+    //Number((pixelSize*10).toFixed(0))
   }
   console.log(`%c colorValues: `, `color: lightgreen`);
   console.log(colorValues);
@@ -100,17 +111,17 @@ function Canvas() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
-    for (let i = 0; i < canvas.width; i++) {
-      for (let j = 0; j < canvas.height; j++) {
-        /* if (colorValues[j] >= 0.8) {
+    for (let i = 0; i < canvas.width/recSize; i++) {
+      for (let j = 0; j < canvas.height/recSize; j++) {
+        /* if (colorValues[j*(i+1)] >= 0.8) {
           ctx.fillStyle = 'rgb(220, 20, 20)';
-        } else if (colorValues[j] < 0.8 && colorValues[j] >= 0.4) {
+        } else if (colorValues[j*(i+1)] < 0.8 && colorValues[j] >= 0.4) {
           ctx.fillStyle = 'rgb(120, 20, 20)';
-        } else if (colorValues[j] < 0.4) {
+        } else if (colorValues[j*(i+1)] < 0.4) {
           ctx.fillStyle = 'rgb(10, 20, 20)';
         } */
 
-        ctx.fillStyle = `rgb(${colorValues[j*(i+1)] * 255}, 20, 20)`;
+        ctx.fillStyle = `rgb(20, ${colorValues[j*(i+1)] * 255}, 20)`;
         ctx.fillRect(i*recSize, j*recSize, recSize, recSize);
       }
     }
@@ -131,7 +142,7 @@ function Canvas() {
         ctx.fillRect(i*recSize, j*recSize, recSize, recSize); */
       }
     }
-    console.log(randomArr);
+    //console.log(randomArr);
 
     for (let i = 0; i < canvasSize; i += canvasSize/8) {
       //console.log(`octavePointsArr ${octavePointsArr}`);
@@ -158,7 +169,7 @@ function Canvas() {
     for (let j = 0; j < canvasSize; j += canvasSize/8) {
       for (let i = 0; i < canvasSize; i += canvasSize/8) {
         if (i === 0 && octavePointsArr.length < 1) {
-          console.log(octavePointsArr);
+          //console.log(octavePointsArr);
         }
 
         
@@ -206,6 +217,12 @@ function App() {
   let gradVectorX = (a * seed + (inc - 1)) % (mod);
   let gradVectorY = (b * seed + (inc - 1)) % (mod);
 
+  if (seedGrad.length < 1) {
+    seedGrad.push([gradVectorX/mod, gradVectorY/mod]);
+  }
+
+
+  //STEP.1: Define random gradient vectors based on seed
   for (let i = inc; i < canvasSize; i++) {
     if (seedGrad.length < canvasSize/(pixelSize*10)) {
       gradVectorX = (a * gradVectorX + i) % (mod); //( ( (seed + i) * ( ( Math.sqrt(i ** i) ) + seed ) ) % 1000 );

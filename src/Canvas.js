ffe7ceb3198dea1a940b2d474e1bcb0907e6640c
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-11 17:44:56 +0100

    Update Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,21 +2,31 @@ import { useEffect, useRef } from 'react';
 
 import './App.css';
 
-function Canvas(props) {
+function Canvas({ width, height}) {
     const canvasRef = useRef(null);
   
     useEffect(() => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
+
+      let size = 8;
   
-      
+      for (let i = 0; i < canvas.width/size; i++) {
+        for (let j = 0; j < canvas.width/size; j++) {
+            let random = Math.floor(Math.random() * 255);
+            console.log(random);
+            ctx.fillStyle = `rgb(${random}, 0, 0)`;
+            ctx.fillRect(i*size, j*size, size, size);
+        }
+      }
+
     }, []);
   
     return (
     <canvas 
         ref={canvasRef} 
-        width={640} 
-        height={640} 
+        width={width} 
+        height={height} 
         className='perlin-canvas'
     ></canvas>
     );

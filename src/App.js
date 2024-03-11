1325c018188035548e51c7efd07960e9f3c5e129
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-11 18:04:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import Plot from 'react-plotly.js';
 
 import Canvas from './Canvas';
 
@@ -13,27 +12,6 @@ function App() {
 
   return (
       <div className='app-container'>
-        <div className='perlin-plot'>
-          <Plot
-            data={[
-              {
-                x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                y: [2.5, 3.1, 4.0, 3.4, 4.2, 5.5, 4.9, 5.3, 5.0, 3.9],
-                type: 'scatter',
-                mode: 'lines',
-                line: {color: 'green'},
-                name: 'Plot',
-              },
-            ]}
-            layout={ {
-              autosize: true, 
-              title: 'Plot',
-              paper_bgcolor: "rgb(157, 192, 194)",
-              plot_bgcolor: "rgb(157, 192, 194)",
-            } }
-            useResizeHandler={true}
-          />
-        </div>
         <Canvas
           width={width}
           height={height}

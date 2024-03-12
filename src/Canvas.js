2a8b14c1ae6da2a4af84c4fb2e54c64776d8721e
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-12 13:27:07 +0100

    Update Canvas.js
    put preview plot data in another file

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,30 +2,17 @@ import { useEffect, useRef } from 'react';
 
 import Plot from 'react-plotly.js';
 
+import * as Perlin from './PerlinFunctions';
+
 import './App.css';
+
+const { setPlotData } = Perlin;
+
 
 function Canvas({ width, height }) {
     const canvasRef = useRef(null);
 
-    let xPlot = [];
-    let yPlot = [];
-
-    let prev = Math.floor((Math.random()*20)-10);
-
-    let plusMinus = [-1, 1];
-
-    for (let i = 1; i < 1000; i++) {
-      xPlot.push(i);
-
-      yPlot.push(prev);
-
-      let indexPM = Math.floor(Math.random()*2);
-      let random = Math.random();
-
-      prev += (plusMinus[indexPM] * random);
-    }
-
-    console.log(yPlot);
+    const axes = setPlotData();
   
     useEffect(() => {
       const canvas = canvasRef.current;
@@ -50,8 +37,8 @@ function Canvas({ width, height }) {
         <Plot
           data={[
             {
-              x: xPlot,
-              y: yPlot,
+              x: axes.xPlot,
+              y: axes.yPlot,
               type: 'scatter',
               mode: 'lines',
               line: {color: 'green'},

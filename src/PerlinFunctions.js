33cf61401c739e2c264bc19d6242786796a18faf
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-13 11:33:59 +0100

    Update PerlinFunctions.js
    added gradient vectors generation (array size later to be fixed)

--- src/PerlinFunctions.js
+++ src/PerlinFunctions.js
@@ -1,4 +1,4 @@
-export function setPlotData() {
+export const setPlotData = () => {
     let xPlot = [];
     let yPlot = [];
 
@@ -14,10 +14,40 @@ export function setPlotData() {
         let indexPM = Math.floor(Math.random()*2);
         let random = Math.random();
 
+        console.log(random);
+
         prev += (plusMinus[indexPM] * random);
     }
 
     console.log(yPlot);
 
     return {xPlot, yPlot};
+}
+
+export const generateGradientVectors = (seed, width, height) => {
+    let gradientArr = [];
+
+    let a = Math.ceil(seed/835);
+    let b = Math.ceil(seed/2906);
+    let mod = 10 ** (seed.toString().length);
+
+    console.log(`${a}, ${b}, ${mod}`);
+
+    let gradientVectorX = (a * seed) % (mod);
+    let gradientVectorY = (b * seed) % (mod);
+
+    console.log(gradientVectorX);
+    console.log(gradientVectorY);
+
+    for (let i = 0; i <= width; i += 8) {
+        for (let j = 0; j <= height; j += 8) {
+            gradientVectorX = (a * gradientVectorX + j) % (mod);
+            gradientVectorY = (b * gradientVectorY + j) % (mod);
+            
+            gradientArr.push([gradientVectorX, gradientVectorY]);
+        }
+        console.log(i)
+    }
+
+    console.log(gradientArr);
 }

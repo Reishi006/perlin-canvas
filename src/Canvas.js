b65468741765a9cfedb6aaa6238898508396f25a
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-14 18:09:52 +0100

    Update Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -23,14 +23,32 @@ function Canvas({ width, height }) {
       const ctx = canvas.getContext('2d');
       const imageData = ctx.createImageData(width, height);
 
-      perlinNoise(seed, width, height, size);
+      const colorValues = perlinNoise(seed, width, height, size);
+      console.log(colorValues.length);
   
       for (let i = 0; i < imageData.data.length; i += 4) {
         let random = Math.floor(Math.random() * 255);
 
-        imageData.data[i] = random;
-        imageData.data[i + 1] = 0 + (random/2);
-        imageData.data[i + 2] = random;
+        /* imageData.data[i] = colorValues[i/4]*255;
+        imageData.data[i + 1] = 0 + ((colorValues[i/4]*255)/2);
+        imageData.data[i + 2] = colorValues[i/4]*255;
+        imageData.data[i + 3] = 255; */
+
+        if (colorValues[i/4] < 0.05) {
+          imageData.data[i] = 100;
+          imageData.data[i + 1] = 0;
+          imageData.data[i + 2] = 100;
+        } else if (colorValues[i/4] >= 0.05 && colorValues[i/4] < 0.4) {
+          imageData.data[i] = 200;
+          imageData.data[i + 1] = 0;
+          imageData.data[i + 2] = 200;
+        } else if (colorValues[i/4] >= 0.4) {
+          imageData.data[i] = 255;
+          imageData.data[i + 1] = 0;
+          imageData.data[i + 2] = 255;
+        }
+
+
         imageData.data[i + 3] = 255;
       }
 

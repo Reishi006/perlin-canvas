808a0fe81b9ccd2e213fc9421c4826235b3606c8
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-03-15 13:21:49 +0100

    Update PerlinFunctions.js
    changed the interpolation formula which (hopefully) finally applied the perlin algorithm correctly
    also, left the old formula commented out just in case if it's needed again

--- src/PerlinFunctions.js
+++ src/PerlinFunctions.js
@@ -66,7 +66,7 @@ export const generateGradientVectors = (seed, width, height, size) => {
 export const getDistanceVectors = (x, y, size) => {
     let distanceArr = [];
 
-    let point = [(x%8)/size+1, (y%8)/size+1];
+    let point = [(x%8)/size, (y%8)/size];
 
     //0 - topLeft, 1 - topRight ... (clockwise)
     distanceArr[0] = [0 - point[0], 0 - point[1]];
@@ -90,18 +90,29 @@ export const getDotProduct = (x, y, gradientArr, distanceArr, size) => {
     return dotProducts;
 }
 
+const doInterpolation = (a0, a1, w) => {
+    return (a1 - a0) * ((w * (w * 6 - 15) + 10) * w * w * w) + a0;
+}
+
 export const interpolateDotProducts = (x, y, dotProducts, size) => {
 
     let point = [(x%size)/size, (y%size)/size];
 
-    let AB = dotProducts[0] + point[0] * (dotProducts[1] - dotProducts[0]);
+    let interpolate_1 = doInterpolation(dotProducts[0], dotProducts[1], point[0]);
+    let interpolate_2 = doInterpolation(dotProducts[3], dotProducts[2], point[0]);
+
+    let result = doInterpolation(interpolate_1, interpolate_2, point[1]);
+
+    /* let AB = dotProducts[0] + point[0] * (dotProducts[1] - dotProducts[0]);
     let CD = dotProducts[3] + point[0] * (dotProducts[2] - dotProducts[3]);
 
     let value = AB + point[1] * (CD - AB);
 
     let result = (6*value**5) - (15*value**4) + (10*value**3);
 
-    result = (result + 2.5) / 5;
+    result = (result + 2.5) / 5; */
+
+    result = result + 0.5;
     
     return result;
 }

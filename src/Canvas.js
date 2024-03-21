f1546e24ab33fdff7920d212dfa36aa7aecf9ab9
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-03-21 19:06:47 +0100

    Update Canvas.js
    moved most of the code into Canvas Component

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
 
 import * as Perlin from './PerlinFunctions';
@@ -10,107 +10,173 @@ const {
 } = Perlin;
 
 
-function Canvas({ width, height }) {
-    const canvasRef = useRef(null);
+function Canvas(props) {
+  const canvasRef = useRef(null);
 
-    const axes = setPlotData(width);
+  const axes = setPlotData(props.width);
 
-    const seed = 800123107341; //800123107341 //782940106259
-    const size = 32;
-
-    const colorValues = perlinNoise(seed, width, height, size);
+  const seed = 800123107341; //800123107341 //782940106259
   
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      const imageData = ctx.createImageData(width, height);
+  const inputRef = useRef(Math.floor(Math.random()*99999999)+100000);
+  const colorValues = useRef(100000);
 
-      console.log(colorValues.length);
-  
-      for (let i = 0; i < imageData.data.length; i += 4) {
-        /* imageData.data[i] = colorValues[i/4]*255;
-        imageData.data[i + 1] = 0 + ((colorValues[i/4]*255)/2);
-        imageData.data[i + 2] = colorValues[i/4]*255;
-        imageData.data[i + 3] = 255; */
+  const [plotValues, setPlotValues] = useState(colorValues.current);
 
-        /* if (colorValues[i/4] < 0.3) {
-          imageData.data[i] = 70;
-          imageData.data[i + 1] = 0;
-          imageData.data[i + 2] = 70;
-        } else if (colorValues[i/4] < 0.5 && colorValues[i/4] >= 0.3) {
-          imageData.data[i] = 100;
-          imageData.data[i + 1] = 0;
-          imageData.data[i + 2] = 100;
-        } else if (colorValues[i/4] >= 0.5 && colorValues[i/4] < 0.7) {
-          imageData.data[i] = 190;
-          imageData.data[i + 1] = 0;
-          imageData.data[i + 2] = 190;
-        } else if (colorValues[i/4] >= 0.7) {
-          imageData.data[i] = 255;
-          imageData.data[i + 1] = 0;
-          imageData.data[i + 2] = 255;
-        } */
+  const size = 128;
 
-        if (colorValues[i/4] <= 0.35) {
-          imageData.data[i] = 10;
-          imageData.data[i + 1] = 10;
-          imageData.data[i + 2] = 170;
-        } else if (colorValues[i/4] > 0.35 && colorValues[i/4] <= 0.55) {
-          imageData.data[i] = 10;
-          imageData.data[i + 1] = 10;
-          imageData.data[i + 2] = 230;
-        } else if (colorValues[i/4] > 0.55 && colorValues[i/4] <= 0.6) {
-          imageData.data[i] = 240;
-          imageData.data[i + 1] = 230;
-          imageData.data[i + 2] = 52;
-        } else if (colorValues[i/4] >= 0.6 && colorValues[i/4] <= 0.75) {
-          imageData.data[i] = 20;
-          imageData.data[i + 1] = 180;
-          imageData.data[i + 2] = 20;
-        } else if (colorValues[i/4] > 0.75) {
-          imageData.data[i] = 30;
-          imageData.data[i + 1] = 210;
-          imageData.data[i + 2] = 30;
-        }
+  colorValues.current = perlinNoise(inputRef.current, props.width, props.height, size);
 
-        imageData.data[i + 3] = 255;
+  const draw = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    //ctx.clearRect(0, 0, props.width, props.height);
+    const imageData = ctx.createImageData(props.width, props.height);
+    console.log(imageData.data.length);
+
+    console.log(colorValues.current.length);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      /* imageData.data[i] = props.colorValues[i/4]*255;
+      imageData.data[i + 1] = 0 + ((props.colorValues[i/4]*255)/2);
+      imageData.data[i + 2] = props.colorValues[i/4]*255;
+      imageData.data[i + 3] = 255; */
+
+      /* if (props.colorValues[i/4] < 0.3) {
+        imageData.data[i] = 70;
+        imageData.data[i + 1] = 0;
+        imageData.data[i + 2] = 70;
+      } else if (props.colorValues[i/4] < 0.5 && props.colorValues[i/4] >= 0.3) {
+        imageData.data[i] = 100;
+        imageData.data[i + 1] = 0;
+        imageData.data[i + 2] = 100;
+      } else if (props.colorValues[i/4] >= 0.5 && props.colorValues[i/4] < 0.7) {
+        imageData.data[i] = 190;
+        imageData.data[i + 1] = 0;
+        imageData.data[i + 2] = 190;
+      } else if (props.colorValues[i/4] >= 0.7) {
+        imageData.data[i] = 255;
+        imageData.data[i + 1] = 0;
+        imageData.data[i + 2] = 255;
+      } */
+
+      if (colorValues.current[i/4] <= 0.35) {
+        imageData.data[i] = 10;
+        imageData.data[i + 1] = 10;
+        imageData.data[i + 2] = 170;
+      } else if (colorValues.current[i/4] > 0.35 && colorValues.current[i/4] <= 0.5) {
+        imageData.data[i] = 10;
+        imageData.data[i + 1] = 10;
+        imageData.data[i + 2] = 230;
+      } else if (colorValues.current[i/4] > 0.5 && colorValues.current[i/4] <= 0.55) {
+        imageData.data[i] = 240;
+        imageData.data[i + 1] = 230;
+        imageData.data[i + 2] = 52;
+      } else if (colorValues.current[i/4] >= 0.55 && colorValues.current[i/4] <= 0.75) {
+        imageData.data[i] = 20;
+        imageData.data[i + 1] = 180;
+        imageData.data[i + 2] = 20;
+      } else if (colorValues.current[i/4] > 0.75) {
+        imageData.data[i] = 30;
+        imageData.data[i + 1] = 210;
+        imageData.data[i + 2] = 30;
       }
 
-      console.log(imageData.data.length);
+      imageData.data[i + 3] = 255;
+    }
 
-      ctx.putImageData(imageData, 0, 0)
+    console.log(imageData.data.length);
+    console.log(colorValues.current);
 
+    ctx.putImageData(imageData, 0, 0);
+
+    setPlotValues(colorValues.current);
+
+  }
+
+    useEffect(() => {
+      draw();
     }, []);
+
+  const getValue = (e) => {
+    e.preventDefault();
+    if (!isNaN(e.target.value)) {
+      inputRef.current = e.target.value; 
+    } else {
+      e.target.value = inputRef.current;
+    }
+    console.log(e.target.value);
+  }
+
+  const generatePerlinKey = (e) => {
+    if (e.key === 'Enter' && inputRef.current.length > 5) {
+      console.log('enter');
+      let seed = Number(inputRef.current);
+      colorValues.current = perlinNoise(seed, props.width, props.height, size);
+      draw();
+    }
+  }
+
+  const generatePerlinButton = () => {
+    if (inputRef.current.length > 5) {
+      console.log('button');
+      let seed = Number(inputRef.current);
+      console.log(seed);
+      colorValues.current = perlinNoise(seed, props.width, props.height, size);
+      draw();
+    }
+  }
   
     return (
     <>
-      <div className='perlin-plot'>
-        <Plot
-          data={[
-            {
-              x: axes.xPlot,
-              y: colorValues,
-              type: 'scatter',
-              mode: 'lines',
-              line: {color: 'green'},
-              name: 'Noise values',
-            },
-          ]}
-          layout={ {
-            autosize: true, 
-            title: 'Noise values',
-            paper_bgcolor: "rgb(137, 192, 174)",
-            plot_bgcolor: "rgb(137, 192, 174)",
-          } }
-          useResizeHandler={true}
-        />
+      <div className='perlin'>
+        <div className='perlin-plot'>
+          <Plot
+            data={[
+              {
+                x: axes.xPlot,
+                y: plotValues,
+                type: 'scatter',
+                mode: 'lines',
+                line: {color: 'green'},
+                name: 'Noise values',
+              },
+            ]}
+            layout={ {
+              autosize: true, 
+              title: 'Noise values',
+              paper_bgcolor: "rgb(137, 192, 174)",
+              plot_bgcolor: "rgb(137, 192, 174)",
+            } }
+            useResizeHandler={true}
+          />
+        </div>
+        <canvas 
+            ref={canvasRef} 
+            width={props.width} 
+            height={props.height} 
+            className='perlin-canvas'
+        ></canvas>
       </div>
-      <canvas 
-          ref={canvasRef} 
-          width={width} 
-          height={height} 
-          className='perlin-canvas'
-      ></canvas>
+      <label className='seed'>
+          Input a seed (number between 100 000 - 999 999 999 999)<br/>
+          <div>
+            <input 
+              className='seed-input' 
+              type='text'
+              min='100000' 
+              max='999999999999'
+              minLength='6'
+              maxLength='12' 
+              placeholder='Enter a seed'
+              onInput={(e) => getValue(e)}
+              onKeyDown={(e) => generatePerlinKey(e)}
+            ></input>
+            <button 
+              className='seed-input-button'
+              onClick={() => generatePerlinButton()}
+            >Enter</button>
+          </div>
+        </label>
     </>
     );
 }
